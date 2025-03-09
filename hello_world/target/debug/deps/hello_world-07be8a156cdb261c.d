3f5c554eb2140eec7ccf964a0e62a0d552d0c35f
Ashin <664463976@qq.com>
2025-03-10 00:55:50 +0800

    change

---
+++ hello_world/target/debug/deps/hello_world-07be8a156cdb261c.d
@@ -0,0 +1,5 @@
+C:\Users\ashin\Desktop\work\rust\hello_world\target\debug\deps\libhello_world-07be8a156cdb261c.rmeta: src\main.rs
+
+C:\Users\ashin\Desktop\work\rust\hello_world\target\debug\deps\hello_world-07be8a156cdb261c.d: src\main.rs
+
+src\main.rs:
