ddc65b057bf9a0bdb054e9763c3429c9d8aac5ad
Ashin <664463976@qq.com>
2025-03-10 01:02:34 +0800

    add ignore

--- hello_world/target/debug/deps/hello_world-af469f0202dc89b2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\ashin\Desktop\work\rust\hello_world\target\debug\deps\libhello_world-af469f0202dc89b2.rmeta: src\main.rs
-
-C:\Users\ashin\Desktop\work\rust\hello_world\target\debug\deps\hello_world-af469f0202dc89b2.d: src\main.rs
-
-src\main.rs:
