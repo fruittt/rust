dcac268cee67202448aa95e05ce362b59ab94b44
zzx <664463976@qq.com>
2025-03-10 17:25:53 +0800

    new project

---
+++ learn_var/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // let num_a: i32 = 45;
+    // let num_b: u32= 45;
+    // let num_c: u8= 255;
+    // let num_d: usize= 46;
+    // let num_e: i32= 0xff;
+    // let num_f: u8= b'A';
+
+    let y = {
+        let x = 5;
+        x + 1
+    };
+    println!("y is {}", y);
+}
